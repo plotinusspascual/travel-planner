ddb676343f371e14bdf5c65e7cbb56f0c5ca0726
plotinusspascual <plotinusspascual@gmail.com>
2022-02-04 12:10:39 -0800

    created Models

---
+++ models/Trips.js
@@ -0,0 +1,39 @@
+const { Model, DataTypes } = require('sequelize');
+const sequelize = require('../config/connection');
+
+class Location extends Model {}
+
+Location.init(
+  {
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true,
+    },
+    trip_budget: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+    },
+    traveller_amount: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+    },
+    traveller_id: {
+      type: DataTypes.INTEGER,
+      references: {
+        model: 'traveller',
+        key: 'id',
+      },
+    },
+  },
+  {
+    sequelize,
+    timestamps: false,
+    freezeTableName: true,
+    underscored: true,
+    modelName: 'location'
+  }
+);
+
+module.exports = Location
